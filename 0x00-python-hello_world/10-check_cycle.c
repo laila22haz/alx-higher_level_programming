728f255fcc9dc5bbaaf11e51f42c06dc71d8b50e
laila22haz <hazmirlaila316@gmail.com>
2023-06-06 01:55:12 +0100

    Python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,21 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *ptr, *temp;
+	listint_t *ptr == NULL;
+	listint_t *temp == NULL;
+
+	if (list == NULL)
+		return (0);
+	ptr = list;
+	temp = list;
 
 	while(ptr != NULL && temp != NULL && temp->next != NULL)
 	{
 		ptr = ptr->next;
 		temp = temp->next->next;
+
 		if (ptr == temp)
 			return (1);
-		else
-			return (0);
 	}
-	return (1);
+	return (0);
 }

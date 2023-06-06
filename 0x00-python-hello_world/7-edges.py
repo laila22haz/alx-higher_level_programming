9f51680deab12d860a64cc28a175e8978d78f446
laila22haz <hazmirlaila316@gmail.com>
2023-06-06 01:05:46 +0100

    Python

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:-6]
+word_first_3 = word[0:3]
 word_first_2 = word[-2:]
 middle_word = word[1:8]
 print(f"First 3 letters: {word_first_3}")

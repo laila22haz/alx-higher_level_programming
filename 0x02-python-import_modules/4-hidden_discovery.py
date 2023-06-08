346251eddd13dd63e10fce1a096175e7595eab3e
laila22haz <hazmirlaila316@gmail.com>
2023-06-08 22:09:26 +0100

    hidden_module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     name = dir(hidden_4)
-    for i in range(name):
+    for i in name:
         if i[:2] != "__":
             print("{}".format(i))

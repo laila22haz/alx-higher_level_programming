51a4089c589c97cd90151f5f2f497aabee1487fc
laila22haz <hazmirlaila316@gmail.com>
2023-06-09 21:41:42 +0100

    Python is cool

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list.copy()
-    my_origin = my_list.copy()
-    my_origin[idx] = element
-    return my_origin
+    new_in_list = my_list.copy()
+    new_in_list[idx] = element
+    return new_in_list

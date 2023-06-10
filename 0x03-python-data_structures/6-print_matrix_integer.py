56c8c771d993914affa5004a91142ffc64a78dbe
laila22haz <hazmirlaila316@gmail.com>
2023-06-11 00:35:47 +0100

    Python is cool

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for colomn in range(len(matrix[row])):
+            if colomn != 0:
+                print(" ", end='')
+            print("{}".format(matrix[row][colomn]), end='')
+        print()

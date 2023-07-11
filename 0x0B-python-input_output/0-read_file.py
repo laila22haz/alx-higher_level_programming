37ffeb2f6cf424624684329d66fc22f869aac455
laila22haz <hazmirlaila316@gmail.com>
2023-07-11 17:31:50 +0100

    cool

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read a file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, mode="r", encoding="UTF-8") as f:
+        f.read()

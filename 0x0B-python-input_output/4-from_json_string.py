270a94204a83a9fd50832c5481a4bdf735f8d968
laila22haz <hazmirlaila316@gmail.com>
2023-07-11 20:41:54 +0100

    cool

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""JSON representation"""
+import json
+
+
+def from_json_string(my_str):
+    """ function that returns the JSON representation of an object"""
+    return json.loads(my_str)

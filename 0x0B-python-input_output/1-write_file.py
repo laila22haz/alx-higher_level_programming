32168058f7c3e6c46304a807f8672b4bb226a132
laila22haz <hazmirlaila316@gmail.com>
2023-07-11 20:17:56 +0100

    cool

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write to a file"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+            return f.write(text)

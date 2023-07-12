e5c9bd8fda8aea6eb77693b757058045d88cc8a5
laila22haz <hazmirlaila316@gmail.com>
2023-07-12 02:10:28 +0100

    cool

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,14 @@ JSON representation
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__(
-    '6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
 try:
     exist_file = load_from_json_file(filename)
-except FileExistsError:
+except FileNotFoundError:
     exist_file = []
 
 save_to_json_file(exist_file + sys.argv[1:], filename)

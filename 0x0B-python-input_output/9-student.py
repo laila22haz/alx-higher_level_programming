fafede6da140fc9587addca722e9103ded05e71a
laila22haz <hazmirlaila316@gmail.com>
2023-07-12 03:40:33 +0100

    cool

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,48 @@
 #!/usr/bin/python3
-""" module """
+# Made by MEGATRON
+"""
+
+Contains class Student
+that initializes public instance attributes first_name,
+last_name, and age, & has public method to_json
+that returns dictionaryrepresentation of requested
+attributes or all if none were requested
+"""
 
 
-class Student:
-    """ class """
+class Student():
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
 
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        Initializes student with full name & age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, str)
+        for JSON serialization of an obj
+
+        Return:
+            Only return dict of attrs given to us
+            Return entire dict if no attrs given
+        """
         if attrs is None:
             return self.__dict__
-        return {key: self.__dict__[key]
-                for key in attrs if key in self.__dict__}
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic

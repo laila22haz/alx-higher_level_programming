09c493f881200751f7ac2e7548c5b296e041665f
laila22haz <hazmirlaila316@gmail.com>
2023-07-12 03:35:19 +0100

    cool

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""build class"""
+""" module """
 
 
 class Student:
-    """class"""
+    """ class """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name

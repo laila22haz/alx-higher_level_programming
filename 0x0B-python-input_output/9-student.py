5e94239a584454852e6cb174bcf67b4150e47a9f
laila22haz <hazmirlaila316@gmail.com>
2023-07-12 03:11:54 +0100

    cool

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""build class"""
+
+
+class Student:
+    """class"""
+    def __init__(self, first_name, last_name, age):
+        first_name = first_name
+        last_name = last_name
+        age = age
+
+    def to_json(self):
+        return self.__dict__

835ac9ea2ccd2d79e4ea475fef6095252420b1ca
laila22haz <hazmirlaila316@gmail.com>
2023-07-12 01:36:58 +0100

    cool

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""JSON representation"""
+'''
+JSON representation
+'''
+
 import sys
 
-
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__
-('6-load_from_json_file.py').load_from_json_file
+load_from_json_file = __import__(
+    '6-load_from_json_file.py').load_from_json_file
 
 filename = "add_item.json"
+
 try:
     exist_file = load_from_json_file(filename)
 except FileExistsError:
     exist_file = []
+
 save_to_json_file(exist_file + sys.argv[1:], filename)

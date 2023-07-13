740a004d909c55b549ca7e1050f114c71a928a0a
laila22haz <hazmirlaila316@gmail.com>
2023-07-13 02:37:37 +0100

    cool

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""build a class"""
+
+
+Rectangle = __import__('9-rectangly').Rectangle
+
+
+class Square(Rectangle):
+    """class building"""
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", size)
+
+    def area(self):
+        "return the area"
+        return self.__width * self.__height
+
+    def __str__(self):
+        "return rectangle description"
+        return "{[:s]} {:d}/{:d}".format(self.__class__.__name__,
+                                        self.__width, self.__height)

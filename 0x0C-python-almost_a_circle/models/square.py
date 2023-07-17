17881d63f64156b9493b5c9f5813bc46809de4e1
laila22haz <hazmirlaila316@gmail.com>
2023-07-17 23:06:46 +0100

    cool

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,10 +16,6 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         """set the size of the Square."""
         self.width = value
         self.height = value

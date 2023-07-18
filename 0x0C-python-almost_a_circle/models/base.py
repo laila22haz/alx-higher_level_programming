0904a26157d1a68340c07a47cca2455af179540f
laila22haz <hazmirlaila316@gmail.com>
2023-07-18 03:29:03 +0100

    cool

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,18 +4,35 @@ import json
 
 
 class Base:
-    """this is the base of all other classes in this project"""
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base.
 
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is not None:
             type(self).id = id
         else:
             type(self).__nb_objects += 1
             type(self).id = type(self).__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)

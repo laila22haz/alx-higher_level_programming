61979da0f6c2a185b8c1f31b9729a20307e6a310
laila22haz <hazmirlaila316@gmail.com>
2023-08-15 22:48:03 +0100

    full creation fix table name

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score int
 );
-INSERT INTO first_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10)
-INSERT INTO first_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES (2, 'Alex', 3)
-INSERT INTO first_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES (3, 'Bob', 14)
-INSERT INTO first_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES (4, 'George', 8)

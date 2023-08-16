bd1471ef200d19557af119418de2c89f68ac009d
laila22haz <hazmirlaila316@gmail.com>
2023-08-16 18:20:58 +0100

    references of foreign id fix 2

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id int NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(state_id)
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );

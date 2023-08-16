943e566bc9d39be4a7e3ba9c1a3021fb7df9d652
laila22haz <hazmirlaila316@gmail.com>
2023-08-16 16:47:48 +0100

    states table frgot unique key word

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
 CREATE TABLE states IF NOT EXISTS (
-    id int DEFAULT 1 UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id int DEFAULT 1 UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256)
 );

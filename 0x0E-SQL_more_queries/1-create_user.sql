73ff5131b7562a430544aaddc3ad3b9697123013
laila22haz <hazmirlaila316@gmail.com>
2023-08-16 14:30:56 +0100

    create a user and grant privilege add *.*

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd'
-GRANT PRIVILEGE ON (*) TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGE ON *.* TO 'user_0d_1'@'localhost'

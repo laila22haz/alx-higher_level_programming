0f49bfabda4a6661a91e2a5d9f963f0d078dde3b
laila22haz <hazmirlaila316@gmail.com>
2023-08-16 18:11:26 +0100

    remove wich db

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,5 @@
 -- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id int NOT NULL FOREIGN KEY,

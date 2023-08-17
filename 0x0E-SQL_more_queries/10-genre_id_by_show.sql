b41eb24e2b2d24e334981b0d2ccc0cfba4fa1dfc
laila22haz <hazmirlaila316@gmail.com>
2023-08-17 18:35:22 +0100

    join genre id

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- dump from hbtn_0d_tvshows to your MySQL server
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

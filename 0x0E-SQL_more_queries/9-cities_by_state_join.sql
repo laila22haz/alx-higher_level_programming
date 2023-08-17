01a83b3c3489695e0519dd825e2d2edacffd2d24
laila22haz <hazmirlaila316@gmail.com>
2023-08-17 14:39:46 +0100

    JOIN cities / state fix  matching column

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states
-ON cities.states_id = states.id
+FROM states
+INNER JOIN cities
+ON cities.id = states.id
 ORDER BY id ASC;

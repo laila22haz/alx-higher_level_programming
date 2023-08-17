a8523bb9eb19615b43e75b615efba2198ea4b74b
laila22haz <hazmirlaila316@gmail.com>
2023-08-17 16:27:11 +0100

    JOIN cities / state fix delete INNER

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM states
-JOIN cities
+INNER JOIN cities
 ON cities.state_id = states.id
 ORDER BY cities.id ASC;

2e6bb1a2b58568f6e07c2914168b125699a162de
laila22haz <hazmirlaila316@gmail.com>
2023-08-17 18:37:54 +0100

    matching key

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

5487ed199d2611e8674640c64b0c8e9a35aa313b
laila22haz <hazmirlaila316@gmail.com>
2023-09-12 00:13:06 +0100

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function add (a, b) {
+  const sum = parseInt(a) + parseInt(b);
+  console.log(sum);
+}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+if (!isNaN(arg1) || !isNaN(arg2)) {
+  add(arg1, arg2);
+}

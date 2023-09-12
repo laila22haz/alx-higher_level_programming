afe18c428c29517fa380a8fe1b46cb720841b829
laila22haz <hazmirlaila316@gmail.com>
2023-09-12 23:40:14 +0100

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0) {
-		this.width = w;
-		this.height = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  print() {
-	for (let index = 0; index < this.height; index++) {
-		let rect = '';
-		for (let j = 0; j < this.width; j++) {
-			rect += 'X';
-		}
-		console.log(rect);
-	}
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      let rect = '';
+      for (let j = 0; j < this.width; j++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
   }
 }
 module.exports = Rectangle;

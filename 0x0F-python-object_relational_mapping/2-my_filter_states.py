821ec7455af4c7c3b5d1f14fa9eeefc11f1dfd00
laila22haz <hazmirlaila316@gmail.com>
2023-09-16 16:07:22 +0100

    2-my_filter_states.py fix sensitive case

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         db=sys.argv[3]
         )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}'\
+    query = "SELECT * FROM states WHERE BINARY name = '{}'\
         ORDER BY states.id ASC".format(sys.argv[4])
     cursor.execute(query)
     my_states = cursor.fetchall()

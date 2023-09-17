5cc5b2cdfe191491e6136d941d3ba963ca7eacc6
laila22haz <hazmirlaila316@gmail.com>
2023-09-17 23:01:31 +0100

    7-model_state_fetch_all.py requirement

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa
+"""
 from model_state import Base, State
 from sqlalchemy import create_engine, select
 import sys

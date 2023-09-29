ec89e4c41d773d3ea298ed49168b30bae2c017b9
laila22haz <hazmirlaila316@gmail.com>
2023-09-30 00:52:33 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""0. What's my status? #0
+"""
+
+import urllib.parse
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+   the_page = response.read()

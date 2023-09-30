5e23121396f05736c2ee55164801c65c917678fa
laila22haz <hazmirlaila316@gmail.com>
2023-09-30 02:03:57 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-from urllib import request
+"""script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        utf_data = data.decode('utf-8')
+        resType = type(data)
+        print(f"Body response:\n\t- type: {resType}\n\t\
+- content: {data}\n\t- utf8 content: {utf_data}")

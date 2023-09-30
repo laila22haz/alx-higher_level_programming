b2fbd75cbf4de2b6879f90100fd4c957658755ad
laila22haz <hazmirlaila316@gmail.com>
2023-09-30 02:01:01 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""0. What's my status? #0
-"""
-
-import urllib.parse
-import urllib.request
-
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    the_page = response.read()
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
 if __name__ == "__main__":
-    pass
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))

69cbc138fce91cdfc19121ddde43db5d2db80df2
laila22haz <hazmirlaila316@gmail.com>
2023-10-24 21:52:31 +0100

    statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, { json: true }, function (error, response, body) {
+  if (error) {
+    return console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
